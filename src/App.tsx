c7155d82f2a7cbaaa104928cde16c59c3df74800
Pinqi Wang <pinqints@gmail.com>
2024-03-28 11:24:52 -0400

    initial commit. api integrated successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MapComponent from "./MapComponent";
+import TechDescripComponent from "./TechDescripComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            CST 8276 Project <br/>
+            <p>How best to store GPS locations for use with Google Map APIs - using Oracle</p>
+        </div>
+        <TechDescripComponent/>
+      <MapComponent/>
+
     </div>
   );
 }
---
+++ src/TechDescripComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const TechDescripComponent: React.FC = () => {
+    return (
+        <div>
+            Api used: Maps datasets API
+
+        </div>
+    );
+};
+
+export default TechDescripComponent;
+
